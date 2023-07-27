2d49cac2a2b48b1f868a3e41cc98c401f8b0f6dc
selonke <bs.brunoselonke@gmail.com>
2023-07-27 11:32:21 +0100

    implemented patch method to Customer

--- spring-6-rest-mvc-5-list-beer/src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
+++ spring-6-rest-mvc-5-list-beer/src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -2,6 +2,8 @@ package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Customer;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -78,5 +80,16 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public void deleteCustomerById(UUID customerId) {
         customerMap.remove(customerId);
+    }
+
+    @Override
+    public void updatePatchCustomerById(UUID customerId, Customer customer) {
+        Customer existing = customerMap.get(customerId);
+
+        if (StringUtils.hasText(customer.getName())){
+            existing.setName(customer.getName());
+        }
+
+        existing.setUpdateDate(LocalDateTime.now());
     }
 }
