17afcb91141118e954ee0ec74749993e81be7bad
selonke <bs.brunoselonke@gmail.com>
2023-08-11 15:31:27 +0100

    created the get customer by id integration test

--- spring-6-rest-mvc-5-list-beer/src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceJPA.java
+++ spring-6-rest-mvc-5-list-beer/src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceJPA.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
 @Service
 @Primary
 @RequiredArgsConstructor
@@ -21,12 +23,14 @@ public class CustomerServiceJPA implements CustomerService {
 
     @Override
     public Optional<CustomerDTO> getCustomerById(UUID id) {
-        return Optional.empty();
+
+        return Optional.ofNullable(customerMapper.customerToCustomerDto(customerRepository.findById(id).orElse(null)));
     }
 
     @Override
     public List<CustomerDTO> getAllCustomers() {
-        return null;
+
+        return customerRepository.findAll().stream().map(customerMapper::customerToCustomerDto).collect(Collectors.toList());
     }
 
     @Override
