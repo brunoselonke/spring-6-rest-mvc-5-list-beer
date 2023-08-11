25af5c2b920d0fa67e6bfb6627cf1873ff664278
selonke <bs.brunoselonke@gmail.com>
2023-08-11 11:44:05 +0100

    Created repository tests

---
+++ spring-6-rest-mvc-5-list-beer/src/test/java/guru/springframework/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,23 @@
+package guru.springframework.spring6restmvc.repositories;
+
+import guru.springframework.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testSaveCustomer() {
+
+        Customer savedCustomer = customerRepository.save(Customer.builder().name("The Customer").build());
+
+        assertThat(savedCustomer).isNotNull();
+        assertThat(savedCustomer.getName()).isNotNull();
+    }
+}
