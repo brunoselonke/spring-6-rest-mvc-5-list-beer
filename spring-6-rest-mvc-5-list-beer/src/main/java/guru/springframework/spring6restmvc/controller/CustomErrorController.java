cba6062af9799a83df06792182ec2d769f369077
selonke <bs.brunoselonke@gmail.com>
2023-08-21 13:55:36 +0100

    added more validation to the beer entity

--- spring-6-rest-mvc-5-list-beer/src/main/java/guru/springframework/spring6restmvc/controller/CustomErrorController.java
+++ spring-6-rest-mvc-5-list-beer/src/main/java/guru/springframework/spring6restmvc/controller/CustomErrorController.java
@@ -1,7 +1,9 @@
 package guru.springframework.spring6restmvc.controller;
 
+import jakarta.validation.ConstraintViolationException;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +15,26 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CustomErrorController {
+
+    @ExceptionHandler
+    ResponseEntity handleJPAViolations(TransactionSystemException exception){
+
+        ResponseEntity.BodyBuilder responseEntity = ResponseEntity.badRequest();
+
+        if(exception.getCause() instanceof ConstraintViolationException){
+            ConstraintViolationException ve = (ConstraintViolationException) exception.getCause().getCause();
+
+            List errors = ve.getConstraintViolations().stream()
+                    .map(constraintViolation -> {
+                      Map<String, String> errMap = new HashMap<>();
+                      errMap.put(constraintViolation.getPropertyPath().toString(), constraintViolation.getMessage());
+                      return errMap;
+                    }).collect(Collectors.toList());
+            return responseEntity.body(errors);
+        }
+
+        return responseEntity.build();
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
         ResponseEntity handleBindErros(MethodArgumentNotValidException exception){
